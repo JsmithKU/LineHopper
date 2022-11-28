fcec633b28df8161ca2349f28ab61c72965a0387
alexrabette <102775751+alexrabette@users.noreply.github.com>
2022-11-27 19:26:30 -0500

    Update server.js
    updated routes... api completed

--- LineHopper/server.js
+++ LineHopper/server.js
@@ -2,18 +2,31 @@
 const express = require('express')
 const db = require('./queries')
 const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
-
+app.use(bodyParser.json())
 
 
 //Initialize routes
 app.get('/uncheckedreports', db.getUncheckedReport)
 
+app.get('/location/:restaurantsid', db.getRestaurant)
+
 app.get('/location', db.getLocation)
 
-//app.post('/uncheckedreports',db.createReport)
+app.get('/uncheckedreports/:reportid', db.getUncheckedReportById)
+
+app.post('/finalizedreports', db.finalizeReport)
+
+app.get('/finalizedreports', db.getFinalizedReport)
+
+app.get('/finalizedReports/:restaurantid', db.getFinalizedReportById)
+
+app.post('/uncheckedreports', db.createReport)
+
+
 //Verify application is running (node api.js)
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
