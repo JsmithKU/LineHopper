15a283e462c518c8437727df9c5881289d6ff624
alexrabette <102775751+alexrabette@users.noreply.github.com>
2022-11-30 09:34:08 -0500

    Added a delete handler *did not test yet*
    add

--- LineHopper/queries.js
+++ LineHopper/queries.js
@@ -1,37 +1,157 @@
-//<config>
-const express = require('express')
-const db = require('./queries')
-const bodyParser = require('body-parser')
+//"Pool" is a collection of queries, connected to our postgresql database.
+const Pool = require('pg').Pool
+const pool = new Pool({
+        //Configs    
+        user: 'postgres',
+        host: 'localhost',
+        database: 'linehopper',
+        password: 'admin',
+        port: 5432
+    })
+//Get a restaurant by it's id 
+const getRestaurant = (req, res) => {
+    const id = req.params.restaurantsid
+    pool.query('select l.restaurantsid, l.address, l.hoursopen, r.name, r.cleanavg, r.busyavg from location as l inner join restaurant as r on (l.restaurantsid = r.restaurantid) where restaurantsid = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        //Send results
+        res.status(200).json(results.rows)
+    })
 
-const app = express()
-const port = 3000
+}
 
-app.use(bodyParser.json())
-//</config>
+//Get ALL unchecked reports 
+const getUncheckedReport = (req, res) => {
+        pool.query('SELECT * from uncheckedReports order by reportid asc', (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
+        })
+    }
+//Get ALL restaurant locations
+const getLocation = (req, res) => {
 
-//<routes>
-app.get('/uncheckedreports', db.getUncheckedReport)
+        pool.query('select l.restaurantsid, l.address, l.hoursopen, r.name, r.cleanavg, r.busyavg from location as l inner join restaurant as r on (l.restaurantsid = r.restaurantid) order by l.restaurantsid', (error, results) => {
 
-app.get('/location/:restaurantsid', db.getRestaurant)
+            if (error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
 
-app.get('/location', db.getLocation)
-
-app.get('/uncheckedreports/:reportid', db.getUncheckedReportById)
-
-app.post('/finalizedreports', db.finalizeReport)
-
-app.get('/finalizedreports', db.getFinalizedReport)
-
-app.get('/finalizedReports/:restaurantid', db.getFinalizedReportById)
-
-app.post('/uncheckedreports', db.createReport)
-
-app.delete('/uncheckedreports',db.deleteUncheckedReport)
+        })
 
 
-//</routes> 
+    }
+//Create a new report 
+const createReport = (req, res) => {
 
-//Verify application is running (node api.js)
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+
+    const { reportid, resturantid, cleanrank, busyrank, picture, submissiontime } = req.body
+
+    pool.query("insert into uncheckedReports(reportid, restaurantid, cleanrank, busyrank, picture, submissiontime) values ($1,$2,$3,$4,$5,$6)", [reportid, resturantid, cleanrank, busyrank, picture, submissiontime],
+
+
+
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.status(201).json(results.rows)
+
+
+        })
+
+}
+
+//Get an unchecked report by id
+const getUncheckedReportById = (req, res) => {
+        const id = req.params.reportid
+        pool.query('select * from uncheckedreports where reportid = $1', [id], 
+        
+        (error, results) => {
+
+            if (error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
+
+        })
+    }
+//Create a finalized report
+const finalizeReport = (req, res) => {
+
+        const { reportid, resturantid, cleanrank, busyrank, picture, submissiontime } = req.body
+
+        pool.query("insert into finalizedreports(reportid, restaurantid, cleanrank, busyrank, picture, submissiontime) values ($1,$2,$3,$4,$5,$6)", [reportid, resturantid, cleanrank, busyrank, picture, submissiontime],
+
+            (error, results) => {
+                if (error) {
+                    throw error
+                }
+                res.status(201).json(results.rows)
+
+
+            })
+
+    }
+//GET ALL finalized reports
+const getFinalizedReport = (req, res) => {
+
+        pool.query('select * from finalizedreports', (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
+        })
+
+    }
+//Get a finalized reports by restaurant id
+const getFinalizedReportById = (req, res) => {
+    const id = req.params.restaurantid
+    pool.query('select * from finalizedreports where restaurantid = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+
+}
+//DELETE an unchecked report
+const deleteUncheckedReport = (req,res) => {
+
+
+const id = req.params.reportid
+
+pool.query('DELETE from uncheckedreports where id = $1', [id], (error, results) => {
+
+
+if (error) {
+    throw error
+}
+res.status(200).json("message:" `Report number ${id} deleted`)
+
+
+
 })
+
+
+
+}
+
+
+module.exports = {
+
+
+    getUncheckedReport,
+    getLocation,
+    createReport,
+    getRestaurant,
+    finalizeReport,
+    getFinalizedReport,
+    getFinalizedReportById,
+    getUncheckedReportById,
+    deleteUncheckedReport
+
+}
