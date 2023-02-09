06ae290b3aede923504429420605e74c3b1cb2a8
JkE <117658094+JsmithKU@users.noreply.github.com>
2023-02-08 19:43:20 -0500

    Created Dockerfiles and composed for DEVENV
    - Created dockerfiles for each layer (Frontend/backend/Database)
    - created composed dockerfile for running each image
    - created sql test dump for testing functions

---
+++ client.old/src/components/LandingPage.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <div class="container">
+    <h2>Restaurant</h2>
+    <div class="" v-for="l in slocation" v-bind:key="l.restaurantsid">
+      <p class="name">{{l.name}}</p>
+      <p>Address: {{l.address}}</p>
+      <p>Hours: {{l.hoursopen}}</p>
+        <locationStat :rid="l.restaurantsid"/>
+      <button class="btn-report" @click="toggleshowform()">Submit Report</button>
+      <addreport :rid="l.restaurantsid" v-show="showForm"  @submit-report="postReport" />
+    </div>
+  </div>
+  <div class="container">
+    <h2>Locations</h2>
+    <div @click="onClick(location.restaurantsid); toggleshowformcloser()" class="locations" v-for="location in locations" v-bind:key="location.restaurantsid">
+      <p class="name">{{location.name}}</p>
+      <p>{{location.address}}</p>
+      <p>{{location.hoursopen}}</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import api from '../api.js';
+import addreport from './addreport.vue';
+import locationStat from './locationstatus.vue';
+
+export default {
+  components: { addreport, locationStat },
+  name: 'LandingPage',
+  props: {
+    msg: String,
+
+  },
+  data(){
+    return {
+      locations: [],
+      slocation:[],
+      error: '',
+      showForm: false,
+    }
+  },
+  async created(){
+    try{
+      this.locations = await api.locations()
+    }catch(err){
+      this.error = "borked."
+    }
+  },
+  methods:{
+
+    async onClick(id){
+      try{
+        this.slocation = await api.getlocation(id)
+      }catch(err){
+        this.error = "borked."
+      }
+    },
+    async postReport(form){
+      try{
+        let formdata = JSON.stringify(form)
+        //console.log(formdata)
+        api.postreport(formdata)
+      }catch(err){
+        this.error = "borked."
+      }
+      this.toggleshowformcloser()
+    },
+    toggleshowform(){
+    this.showForm = !this.showForm
+    },
+    toggleshowformcloser(){
+    this.showForm = false // really bad way of closing on new location selection
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.container{
+  border: 2px solid black;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  align-items: center;
+  justify-content: center;
+}
+.locations{
+  border: 2px solid black;
+  float: left;
+  width: 150px;
+  height: 300px;
+  margin: 10px 10px;
+}
+.name{
+  font-size: 1.8rem;
+}
+.btn-report{
+  margin: 10px 10px;
+}
+</style>
---
+++ client.old/src/views/trustedView.vue
@@ -0,0 +1,17 @@
+<template>
+<router-link to="/"> Normal View </router-link>
+<adminPage msg="Trusted View"/>
+
+</template>
+
+
+<script>
+import adminPage from '../components/adminPage.vue'
+
+export default{
+  name: 'trustedView',
+  components: {
+    adminPage, 
+  },
+}
+</script>
