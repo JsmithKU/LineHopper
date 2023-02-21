293fcd04b77f14aa6cd37bc55a1ec7d8e9cff07b
alexrabette <102775751+alexrabette@users.noreply.github.com>
2023-02-21 17:46:22 -0500

    Added search location to API
    Added search location to API -- returning restaurant name and restaurant ID -- search by restaurantname

---
+++ jwt-helper.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+
+function jwtTokens({userid,email}) {
+  const user = {userid,email}
+  const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'15m'})
+  const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET,{expiresIn:'14d'})
+  return ({accessToken,refreshToken})
+}
+
+module.exports = {
+  jwtTokens,
+
+}
