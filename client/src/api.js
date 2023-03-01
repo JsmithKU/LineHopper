81e5cf0a1f9a9380b8d25049457b0ba97f41c997
alexrabette <102775751+alexrabette@users.noreply.github.com>
2023-02-28 20:01:33 -0500

    Added api functions for create account
    Allows account creation.

--- client/src/api.js
+++ client/src/api.js
@@ -2,129 +2,146 @@ import axios from 'axios';
 
 const url = 'http://localhost:3000/api/'
 
-class api{
-  // get content 
-  // static getLocations(){
-  //   return new Promise((resolve, reject) =>{
-  //     try{
-  //       const res = axios.get(`${url}`)
-  //       console.log(res)
-  //       const data = res.then((r))
-  //       console.log(data)
-  //       resolve(
-  //         data.map(location => ({
-  //           ...location
-  //         }))
-  //       )
-  //     } catch(err){
-  //       reject(err)
-  //     }
-  //   })
-  // }
-  static locations(){
-      // create a promise for the axios request
-      const promise = axios.get(`${url}location/`)
+class api {
+    // get content 
+    // static getLocations(){
+    //   return new Promise((resolve, reject) =>{
+    //     try{
+    //       const res = axios.get(`${url}`)
+    //       console.log(res)
+    //       const data = res.then((r))
+    //       console.log(data)
+    //       resolve(
+    //         data.map(location => ({
+    //           ...location
+    //         }))
+    //       )
+    //     } catch(err){
+    //       reject(err)
+    //     }
+    //   })
+    // }
+    static locations() {
+        // create a promise for the axios request
+        const promise = axios.get(`${url}location/`)
 
-      // using .then, create a new promise which extracts the data
-      const dataPromise = promise.then((response) => response.data)
-  
-      // return it
-      return dataPromise
-  }
-  static getlocation(id){
-    // create a promise for the axios request
-    const promise = axios.get(`${url}location/${id}`)
+        // using .then, create a new promise which extracts the data
+        const dataPromise = promise.then((response) => response.data)
 
-    // using .then, create a new promise which extracts the data
-    const dataPromise = promise.then((response) => response.data)
+        // return it
+        return dataPromise
+    }
+    static getlocation(id) {
+            // create a promise for the axios request
+            const promise = axios.get(`${url}location/${id}`)
 
-    // return it
-    return dataPromise
-}
-  // post content
-  static postreport(form){
-    const res = axios.post(`${url}uncheckedreports`,form, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return res.data.data
-  }
+            // using .then, create a new promise which extracts the data
+            const dataPromise = promise.then((response) => response.data)
 
-  static uncheckedreports(){
-    // create a promise for the axios request
-    const promise = axios.get(`${url}uncheckedreports/`)
+            // return it
+            return dataPromise
+        }
+        // post content
+    static postreport(form) {
+        const res = axios.post(`${url}uncheckedreports`, form, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return res.data.data
+    }
 
-    // using .then, create a new promise which extracts the data
-    const dataPromise = promise.then((response) => response.data)
+    static uncheckedreports() {
+        // create a promise for the axios request
+        const promise = axios.get(`${url}uncheckedreports/`)
 
-    // return it
-    return dataPromise
-}
-  static uncheckreportbyid(id){
+        // using .then, create a new promise which extracts the data
+        const dataPromise = promise.then((response) => response.data)
+
+        // return it
+        return dataPromise
+    }
+    static uncheckreportbyid(id) {
         // create a promise for the axios request
         const promise = axios.get(`${url}uncheckedreports/${id}`)
 
         // using .then, create a new promise which extracts the data
         const dataPromise = promise.then((response) => response.data)
-    
+
         // return it
         return dataPromise
-  }
-  static deleteunchecked(id){
-    const res = axios.delete(`${url}uncheckedreports/${id}`)
-    return res.status
-  }
+    }
+    static deleteunchecked(id) {
+        const res = axios.delete(`${url}uncheckedreports/${id}`)
+        return res.status
+    }
 
-  static approvereport(form){
-    const res = axios.post(`${url}finalizedreports`,form, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return res.data
-  }
-  
-  static getlocationstat(id){
-            // create a promise for the axios request
-            const promise = axios.get(`${url}locationstats/${id}`)
+    static approvereport(form) {
+        const res = axios.post(`${url}finalizedreports`, form, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return res.data
+    }
 
-            // using .then, create a new promise which extracts the data
-            const dataPromise = promise.then((response) => response.data)
-        
-            // return it
-            return dataPromise
-  }
+    static getlocationstat(id) {
+        // create a promise for the axios request
+        const promise = axios.get(`${url}locationstats/${id}`)
 
-  static getlocationlatest(id){
-    // create a promise for the axios request
-    const promise = axios.get(`${url}locationlatest/${id}`)
+        // using .then, create a new promise which extracts the data
+        const dataPromise = promise.then((response) => response.data)
 
-    // using .then, create a new promise which extracts the data
-    const dataPromise = promise.then((response) => response.data)
+        // return it
+        return dataPromise
+    }
 
-    // return it
-    return dataPromise
-}
+    static getlocationlatest(id) {
+        // create a promise for the axios request
+        const promise = axios.get(`${url}locationlatest/${id}`)
 
-  static getlocationdowreport(id){
+        // using .then, create a new promise which extracts the data
+        const dataPromise = promise.then((response) => response.data)
+
+        // return it
+        return dataPromise
+    }
+
+    static getlocationdowreport(id) {
         // create a promise for the axios request
         const promise = axios.get(`${url}locationdow/${id}`)
 
         // using .then, create a new promise which extracts the data
         const dataPromise = promise.then((response) => response.data)
-    
+
         // return it
         return dataPromise
-  }
-  static login(email,password){
-    const promise = axios.post(`${url}login`,{
-    email,password
-    })
-    const dataPromise = promise.then((response) => response.data)
+    }
+    static login(email, password) {
+        const promise = axios.post(`${url}login`, {
+            email,
+            password
+        })
+        const dataPromise = promise.then((response) => response.data)
 
-    return dataPromise
-  }
+        return dataPromise
+    }
+
+    static signUp(email, password) {
+
+        const promise = axios.post(`${url}useraccount`, {
+
+            email,
+            password
+        })
+        const dataPromise = promise.then((response) => response.data)
+        return dataPromise
+
+    }
+
 }
+
+
+
 
 export default api
