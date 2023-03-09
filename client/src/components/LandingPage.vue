297929390a19cc7d58965dc4744e5301b375e433
JkE <117658094+JsmithKU@users.noreply.github.com>
2023-03-08 19:10:44 -0500

    Search Attempt Code

--- client/src/components/LandingPage.vue
+++ client/src/components/LandingPage.vue
@@ -21,7 +21,7 @@
     </div>
   </div>
   <div class="container">
-    <search />
+    <search :searchList="locationSearch"/>
   </div>
   <div class="container">
     <h2>Locations</h2>
@@ -83,9 +83,9 @@ export default {
 
       //this.locationSearch = await api.locationarray()
       this.locations.data.forEach((element) =>
-        this.locationSearch.push(element.restaurantname)
+        this.locationSearch.push(element)
       );
-      //console.log(this.locationSearch)
+      console.log(this.locationSearch)
     } catch (err) {
       this.error = "borked.";
     }
@@ -154,56 +154,5 @@ a {
 }
 .btn-report {
   margin: 10px 10px;
-}
-
-.autocomplete {
-  position: relative;
-  display: inline-block;
-}
-input {
-  border: 1px solid transparent;
-  background-color: #f1f1f1;
-  padding: 10px;
-  font-size: 16px;
-}
-
-input[type="text"] {
-  background-color: #f1f1f1;
-  width: 100%;
-}
-
-input[type="submit"] {
-  background-color: DodgerBlue;
-  color: #fff;
-  cursor: pointer;
-}
-.autocomplete-items {
-  position: absolute;
-  border: 1px solid #d4d4d4;
-  border-bottom: none;
-  border-top: none;
-  z-index: 99;
-  /*position the autocomplete items to be the same width as the container:*/
-  top: 100%;
-  left: 0;
-  right: 0;
-}
-
-.autocomplete-items div {
-  padding: 10px;
-  cursor: pointer;
-  background-color: #fff;
-  border-bottom: 1px solid #d4d4d4;
-}
-
-/*when hovering an item:*/
-.autocomplete-items div:hover {
-  background-color: #e9e9e9;
-}
-
-/*when navigating through the items using the arrow keys:*/
-.autocomplete-active {
-  background-color: DodgerBlue !important;
-  color: #ffffff;
 }
 </style>
--- client/src/components/search.vue
+++ client/src/components/search.vue
@@ -7,12 +7,95 @@
           type="text"
           name="locationSearch"
           placeholder="Restaurant..."
+          @input="showResults(this.value)"
         />
       </div>
       <input type="submit" v-show="true" />
+      <ul>
+        <div id="searchResult" v-for="location in locations" v-bind:key="location.restaurantid">
+          <li> {{ location.restaurantname }}</li>
+        </div>
+      </ul>
     </form>
 </template>
 
 <script>
+export default {
+  name: "search",
+  props:{
+    searchList: Object,
 
+  },
+  data(){
+    return{
+      locations: [],
+    }
+  },
+  async created(){
+    console.log(this.searchList)
+    // this.searchList.data.forEach((element) =>
+    //   this.locations.push(element)
+    // );
+    // console.log(this.searchList)
+    // console.log(this.locations)
+  },
+  methods:{
+    async showResults(values){
+      return values
+    }
+  }
+}
 </script>
+
+<style scoped>
+.autocomplete {
+  position: relative;
+  display: inline-block;
+}
+input {
+  border: 1px solid transparent;
+  background-color: #f1f1f1;
+  padding: 10px;
+  font-size: 16px;
+}
+
+input[type="text"] {
+  background-color: #f1f1f1;
+  width: 100%;
+}
+
+input[type="submit"] {
+  background-color: DodgerBlue;
+  color: #fff;
+  cursor: pointer;
+}
+.autocomplete-items {
+  position: absolute;
+  border: 1px solid #d4d4d4;
+  border-bottom: none;
+  border-top: none;
+  z-index: 99;
+  /*position the autocomplete items to be the same width as the container:*/
+  top: 100%;
+  left: 0;
+  right: 0;
+}
+
+.autocomplete-items div {
+  padding: 10px;
+  cursor: pointer;
+  background-color: #fff;
+  border-bottom: 1px solid #d4d4d4;
+}
+
+/*when hovering an item:*/
+.autocomplete-items div:hover {
+  background-color: #e9e9e9;
+}
+
+/*when navigating through the items using the arrow keys:*/
+.autocomplete-active {
+  background-color: DodgerBlue !important;
+  color: #ffffff;
+}
+</style>
