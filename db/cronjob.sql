4df199f88e71d1ea12204086ff4c47bb8c14a75a
ChristopherCohen <74074016+ChristopherCohen@users.noreply.github.com>
2023-04-10 14:13:37 -0400

    Update cronjob.sql

--- db/cronjob.sql
+++ db/cronjob.sql
@@ -1,29 +1,31 @@
 --Increment month value by 1:
 
 SELECT cron.schedule (
-'5 0 1 1-12 *',
-UPDATE avgarchive
-set monthcount = monthcount + 1
-SELECT monthcount + 1;
-)
+--'5 0 1 1-12 *',
+'* * * * *',
+$$UPDATE avgarchive
+set monthcount = monthcount + 1$$
+);
 --Might have to rewrite as "set monthcount = row(monthcount + 1)"
 
 --Delete data with month value > 5:
 
 SELECT cron.schedule (
-'10 0 1 1-12 *',
-DELETE FROM avgarchive
-where monthcount > 5;
-)
+--'10 0 1 1-12 *',
+'* * * * *',
+$$DELETE FROM avgarchive
+where monthcount > 5$$
+);
 
 --Insert into archive table: 
 
 SELECT cron.schedule (
-'15 0 1 1-12 *',
-INSERT INTO avgarchive (restaurantid, monthcount, monthclean, monthbusy)
+--'15 0 1 1-12 *',
+'* * * * *',
+$$INSERT INTO avgarchive (restaurantid, monthcount, monthclean, monthbusy)
 SELECT restaurantid, 1, cleanavg, busyavg
-FROM restaurant;
-)
+FROM restaurant$$
+);
 --
 --(consider changing "archid" from "bigint" to "serial" for incrementing id value)
 --(make "monthcount" a default value of '1')
