64665f768c772c294453aedaa19e9ce1d62f874f
JkE <117658094+JsmithKU@users.noreply.github.com>
2023-04-12 19:01:50 -0400

    Fixed relational db issues
    added connection to linehop schema.

--- api/dbconnector.js
+++ api/dbconnector.js
@@ -30,74 +30,74 @@ const pool = new Pool(containerPool) // poolConfig)
 
 //Forgot password 
 let forgotPassword = (`
-UPDATE useraccount 
+UPDATE linehop.useraccount 
 set password = $1,
     usercode = null
 where email = $2`);
 
 let restaurantGET = (`
 select l.restaurantid, l.address, r.restaurantname, r.cleanavg, r.busyavg 
-from location as l 
-  inner join restaurant as r using(restaurantid)
+from linehop.location as l 
+  inner join linehop.restaurant as r using(restaurantid)
 where restaurantid = $1
 `)
 let restaurantNameGET = (`
 select restaurantid 
-from restaurant
+from linehop.restaurant
 where restaurantname = $1
 `)
 
 let untrustedreportGET = (`
 SELECT ur.reportid, ur.restaurantid, ur.cleanrank, ur.busyrank, ur.picture, r.restaurantname, ur.submissiontime, ur.userid 
-from reports as ur 
-  inner join restaurant as r using(restaurantid)
+from linehop.reports as ur 
+  inner join linehop.restaurant as r using(restaurantid)
 where ur.trusted IS NOT TRUE
 order by reportid asc
 `)
 
 let locationGET = (`
 select l.restaurantid, l.address, l.openhour, l.closehour, r.restaurantname, r.cleanavg, r.busyavg 
-from location as l 
-  inner join restaurant as r using(restaurantid) 
+from linehop.location as l 
+  inner join linehop.restaurant as r using(restaurantid) 
 order by l.restaurantid
 `)
 
 let reportCreate = (`
-insert into reports(reportid, restaurantid, cleanrank, busyrank, picture, submissiontime, userid, trusted) 
+insert into linehop.reports(reportid, restaurantid, cleanrank, busyrank, picture, submissiontime, userid, trusted) 
 values ($1,$2,$3,$4,$5,$6,$7,$8)
 `)
 
 let reportGetOne = (`
-select * from reports where reportid = $1
+select * from linehop.reports where reportid = $1
 `)
 
 let reportTrust = (`
-UPDATE reports
+UPDATE linehop.reports
 SET(trusted) = ROW(true)
 WHERE reportid = $1;
 `)
 
 let reportTrustedGet = (`
-select * from reports where trusted is True
+select * from linehop.reports where trusted is True
 `)
 
 
 let untrustedreportDelete = (`
-DELETE from reports where reportid = $1 
+DELETE from linehop.reports where reportid = $1 
 `)
 
 let getStat = `
 with reportsum as (
     select restaurantid, sum(cleanrank) as cleansum,
            sum(busyrank) as busysum
-    from reports
+    from linehop.reports
       where trusted is True AND date_trunc('week',submissiontime) >= 
             date_trunc('week',CURRENT_TIMESTAMP) - interval '1 week'
     group by restaurantid
     ),
     counter as (
     select restaurantid, count(*) as totalreports
-    from reports
+    from linehop.reports
       where trusted is True AND date_trunc('week',submissiontime) >= 
             date_trunc('week',CURRENT_TIMESTAMP) - interval '1 week'
     group by restaurantid
@@ -108,9 +108,9 @@ with reportsum as (
       ROUND(rs.busysum / (c.totalreports),2) as busyavg,
       ROUND((((rs.cleansum /c.totalreports) * 5)) / 2, 2) as algclean,
       ROUND((((rs.busysum /c.totalreports) * 5)) / 2, 2) as algbusy
-    from reportsum as rs
-      inner join counter as c using (restaurantid)
-      inner join reports as fr using (restaurantid)
+    from linehop.reportsum as rs
+      inner join linehop.counter as c using (restaurantid)
+      inner join linehop.reports as fr using (restaurantid)
     where rs.restaurantid = $1 and fr.trusted is True
     group by rs.restaurantid, cleanavg, busyavg, algclean, algbusy
     ;
@@ -119,7 +119,7 @@ let getLatest = `
 with latest as (
     select restaurantid, cleanrank, busyrank, submissiontime,
       rank() OVER (PARTITION BY restaurantid order by submissiontime desc) as counter
-    from reports
+    from linehop.reports
     where trusted is True 
     order by submissiontime 
   )
@@ -128,7 +128,7 @@ with latest as (
     to_char(submissiontime, 'DDth') as sday,
     to_char(submissiontime, 'HH24') as shour,
     to_char(submissiontime, 'MI') as sminute
-  from latest 
+  from linehop.latest 
   where counter = 1 AND 
         restaurantid = $1
   order by submissiontime desc;
@@ -138,7 +138,7 @@ with reportsCTE as(
 select restaurantid, cleanrank as crank, busyrank as brank, submissiontime as stime,
   to_char(submissiontime, 'ID') as dowtime, -- Monday(1) Sunday(7)
   to_char(submissiontime, 'HH24') as timeofday -- 00 - 24 | 05 - 11 Breakfast, 11 - 17 Lunch, 18 - 22 Dinner, 23-04 Other
-from reports
+from linehop.reports
 where trusted is True
 ),
 countday as (
@@ -146,44 +146,44 @@ countday as (
     round(avg(crank),2) as cleanavg, 
     round(avg(brank),2) as busyavg,
     dowtime
-  from reportsCTE
+  from linehop.reportsCTE
   group by dowtime, restaurantid
   order by restaurantid, dowtime
 )
 select restaurantid, cleanavg, busyavg, dowtime, to_char(CURRENT_TIMESTAMP, 'Day') as cday
-from countday
+from linehop.countday
 where dowtime = to_char(CURRENT_TIMESTAMP, 'ID') AND restaurantid = $1;
 `;
 let historyweight = `
-select * from avgarchive where restaurantid = $1;
+select * from linehop.avgarchive where restaurantid = $1;
 `;
-let createUser = `insert into useraccount (email, password, usercode) values ($1,$2,$3) `;
+let createUser = `insert into linehop.useraccount (email, password, usercode) values ($1,$2,$3) `;
 
-let getUser = `select * from useraccount where email = $1`;
+let getUser = `select * from linehop.useraccount where email = $1`;
 
-let searchLocation = `select restaurantname, restaurantid from restaurant where restaurantname = $1`;
+let searchLocation = `select restaurantname, restaurantid from linehop.restaurant where restaurantname = $1`;
 
-let checkrole = `select role from useraccount where userid = $1`;
-let verifyrole = `select role from useraccount where email = $1`;
-let codecompare = 'select usercode from useraccount where email = $1';
-let trustcompare = 'select usercode from useraccount where userid = $1';
+let checkrole = `select role from linehop.useraccount where userid = $1`;
+let verifyrole = `select role from linehop.useraccount where email = $1`;
+let codecompare = 'select usercode from linehop.useraccount where email = $1';
+let trustcompare = 'select usercode from linehop.useraccount where userid = $1';
 let updaterole = (`
-UPDATE useraccount
+UPDATE linehop.useraccount
 SET role = 'user'
 WHERE email = $1 AND usercode = $2
 `)
 let updatecode = (`
-UPDATE useraccount
+UPDATE linehop.useraccount
 set usercode = $2
 where email = $1
 `)
 
 let restaurantCreate = (`
-insert into restaurant(restaurantid, restaurantname) 
+insert into linehop.restaurant(restaurantid, restaurantname) 
 values ($1,$2)
 `)
 let locationCreate = (`
-insert into location(locationid, restaurantid, address, openhour, closehour) 
+insert into linehop.location(locationid, restaurantid, address, openhour, closehour) 
 values ($1,$2,$3,$4,$5)
 `)
 
